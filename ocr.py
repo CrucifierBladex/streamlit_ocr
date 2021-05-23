b158845b67ea78b4f50602154f09c4a7632fd9af
Adil Aziz <adila.17.beee@acharya.ac.in>
2021-05-23 09:33:03 +0530

    Add files via upload

---
+++ ocr.py
@@ -0,0 +1,22 @@
+import cv2
+import pytesseract
+import streamlit as st
+from PIL import Image
+st.image('cover.png')
+st.title('OCR APP')
+st.subheader('Developed by Adil')
+st.write('This app is under Development')
+file=st.file_uploader('Upload a File',type=['png','jpeg'])
+
+
+if file is None:
+	st.write('No file uploaded')
+else:
+	convert_img=Image.open(file).convert('RGB')
+	st.header('Uploded Image')
+	st.image(convert_img,width=400)
+	custom_config = r'--oem 3 --psm 6'
+	if st.button('Perform OCR'):
+		res=pytesseract.image_to_string(convert_img, config=custom_config)
+		st.header('Output')
+		st.write(res)
